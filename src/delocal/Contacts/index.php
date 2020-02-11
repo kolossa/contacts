2938c494f4b7cc49e8435a4a8234626df0b49d30
Kolossa Márton <kolosssa@gmail.com>
2020-02-11 21:36:20 +0100

    base dir definiálása

--- src/delocal/Contacts/index.php
+++ src/delocal/Contacts/index.php
@@ -1,7 +1,9 @@
-<?php 
-require_once( __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php' );
+<?php
+define("BASE_DIR", __DIR__);
 
-$routes=require_once(__DIR__ . DIRECTORY_SEPARATOR . 'routing' . DIRECTORY_SEPARATOR . 'routes.php');
+require_once( BASE_DIR . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php' );
+
+$routes=require_once(BASE_DIR . DIRECTORY_SEPARATOR . 'routing' . DIRECTORY_SEPARATOR . 'routes.php');
 
 $frontController=new delocal\Contacts\App\FrontController($routes);
 
