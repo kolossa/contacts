b76d34c354ca87bbbed9ababa49771ff17841318
Kolossa Márton <kolossa.marton@utazom.com>
2020-02-11 14:58:14 +0100

    routing config file

---
+++ src/delocal/Contacts/routing/routes.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * routing array
+ *
+ * The key of this associative array is the route itself.
+ * In the route you can set parametes inside braces.
+ * The value of a route is an associative array with a keyword 'controller' key which contains the 
+ * controller class name with full namespace and a keyword 'action' key which is the action method name of 
+ * a controller class.
+ */
+return [
+	'test/{id}'=>['controller'=>'delocal\Contacts\Controllers\TestController', 'action'=>'actionIndex'],
+];
