e8ff15e7221f541ce0181b1f6e4470f37b3586cb
Kolossa Márton <kolosssa@gmail.com>
2020-02-11 22:38:13 +0100

    create controller route

--- src/delocal/Contacts/routing/routes.php
+++ src/delocal/Contacts/routing/routes.php
@@ -1,14 +1,15 @@
-<?php 
+<?php
 
 /**
  * routing array
  *
  * The key of this associative array is the route itself.
- * In the route you can set parametes inside braces.
- * The value of a route is an associative array with a keyword 'controller' key which contains the 
- * controller class name with full namespace and a keyword 'action' key which is the action method name of 
+ * In the route you can set parameters inside braces.
+ * The value of a route is an associative array with a keyword 'controller' key which contains the
+ * controller class name with full namespace and a keyword 'action' key which is the action method name of
  * a controller class.
  */
 return [
-	'test/{id}'=>['controller'=>'delocal\Contacts\Controllers\TestController', 'action'=>'actionIndex'],
+    'test/{id}' => ['controller' => 'delocal\Contacts\Controllers\TestController', 'action' => 'actionIndex'],
+    'contacts/create' => ['controller' => 'delocal\Contacts\Controllers\CreateContactController', 'action' => 'actionIndex'],
 ];
