4ff3c027621f1c4915d9b990d700bd7ce08ea814
Kolossa Márton <kolossa.marton@utazom.com>
2020-02-11 16:14:08 +0100

    route - url összehasonlítás után a megfelelő controller nevének visszaadása

--- src/delocal/Contacts/App/FrontController.php
+++ src/delocal/Contacts/App/FrontController.php
@@ -4,5 +4,53 @@ namespace delocal\Contacts\App;
 
 class FrontController{
 	
+	protected $routes;
+	
+	public function __construct($routes){
+		
+		$this->routes=$routes;
+	}
+	
+	public function findControllerByUrl(string $url) {
+		
+		$urlInParts=explode('/', $url);
+		$filteredUrlInParts=array_filter($urlInParts);
+		
+		foreach($this->routes as $route=>$values){
+			
+			$routeInParts=explode('/', $route);
+			$filteredRouteInParts=array_filter($routeInParts);
+			
+			$isRouteMatchWithUrl=true;
+			end($filteredRouteInParts);
+			end($filteredUrlInParts);
+			
+			for($i=0;$i<count($filteredRouteInParts);$i++){
+				
+				$partRoute=current($filteredRouteInParts);
+				$partUrl=current($filteredUrlInParts);
+				
+				
+				if(stripos($partRoute, '{')!==false){
+					
+					continue;
+				}
+				
+				if($partRoute!=$partUrl){
+					$isRouteMatchWithUrl=false;
+					break;
+				}
+				
+				prev($filteredRouteInParts);
+				prev($filteredUrlInParts);
+			}
+			
+			if($isRouteMatchWithUrl){
+				
+				return $values;
+			}
+		}
+	}
+	
 	public function run() {}
 }
