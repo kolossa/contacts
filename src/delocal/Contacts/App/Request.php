53c49d9b4149ea2ace7fb92f1bf98b94de412414
Kolossa Márton <kolosssa@gmail.com>
2020-02-11 22:17:22 +0100

    content type lekérése

--- src/delocal/Contacts/App/Request.php
+++ src/delocal/Contacts/App/Request.php
@@ -14,15 +14,25 @@ class Request
 
     public function __construct(array $get = [], array $post = [], array $cookies = [], array $files = [], array $server = [])
     {
-        $this->get=$get;
-        $this->post=$post;
-        $this->cookies=$cookies;
-        $this->files=$files;
-        $this->server=$server;
+        $this->get = $get;
+        $this->post = $post;
+        $this->cookies = $cookies;
+        $this->files = $files;
+        $this->server = $server;
     }
 
     public function isPutRequest()
     {
-        return (isset($this->server['REQUEST_METHOD']) && !strcasecmp($this->server['REQUEST_METHOD'],'PUT'));
+        return (isset($this->server['REQUEST_METHOD']) && !strcasecmp($this->server['REQUEST_METHOD'], 'PUT'));
+    }
+
+    public function getContentType()
+    {
+        if (isset($this->server["CONTENT_TYPE"])) {
+            return $this->server["CONTENT_TYPE"];
+        } elseif (isset($this->server["HTTP_CONTENT_TYPE"])) {
+            return $this->server["HTTP_CONTENT_TYPE"];
+        }
+        return null;
     }
 }
