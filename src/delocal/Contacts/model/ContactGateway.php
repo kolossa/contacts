650ed575bfa21f35b167dad8ddbfeca8b500283b
Kolossa Márton <kolossa.marton@utazom.com>
2020-02-12 16:52:33 +0100

    exists metódus alapok

--- src/delocal/Contacts/model/ContactGateway.php
+++ src/delocal/Contacts/model/ContactGateway.php
@@ -6,10 +6,27 @@ namespace delocal\Contacts\model;
 
 class ContactGateway extends TableDataGateway
 {
+	protected $tableName='contacts';
+
     public function insert(ContactGatewayDTO $dto): bool
     {
-        $sql = "INSERT INTO contacts (name, email, phone_number, address), VALUES(?,?,?,?)";
+        $sql = "INSERT INTO $this->tableName (name, email, phone_number, address), VALUES(?,?,?,?)";
         $stmt = $this->pdo->prepare($sql);
         return $stmt->execute([$dto->getName(), $dto->getEmail(), $dto->getPhoneNumber(), $dto->getAddress()]);
     }
+
+    public function exists(ContactGatewayDTO $dto): bool
+	{
+		$sql = "SELECT * FROM $this->tableName WHERE name=? COLLATE utf8_bin AND email=? AND phone_number=? AND address=? COLLATE utf8_bin LIMIT 1";
+		$stmt = $this->pdo->prepare($sql);
+		$stmt->execute([$dto->getName(), $dto->getEmail(), $dto->getPhoneNumber(), $dto->getAddress()]);
+
+		$result = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+		if($result==false){
+			return false;
+		}
+
+		return true;
+	}
 }
