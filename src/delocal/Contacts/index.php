3edd3c7d8a402924b5203d7c786675e457decf43
Kolossa Márton <kolosssa@gmail.com>
2020-02-13 20:08:45 +0100

    init-be kiemelése az importálandó fájloknak + alap beállításoknak

--- src/delocal/Contacts/index.php
+++ src/delocal/Contacts/index.php
@@ -1,11 +1,9 @@
 <?php
-define("BASE_DIR", __DIR__);
+require_once('init.php');
 
-require_once( BASE_DIR . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php' );
+$routes = require_once(BASE_DIR . DIRECTORY_SEPARATOR . 'routing' . DIRECTORY_SEPARATOR . 'routes.php');
 
-$routes=require_once(BASE_DIR . DIRECTORY_SEPARATOR . 'routing' . DIRECTORY_SEPARATOR . 'routes.php');
-
-$frontController=new delocal\Contacts\App\FrontController($routes);
+$frontController = new delocal\Contacts\App\FrontController($routes);
 
 $frontController->run($_SERVER['REQUEST_URI']);
 
---
+++ src/delocal/Contacts/init.php
@@ -0,0 +1,5 @@
+<?php
+
+define("BASE_DIR", __DIR__);
+
+require_once( BASE_DIR . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php' );
